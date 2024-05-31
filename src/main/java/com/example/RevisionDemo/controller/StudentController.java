13f09c830d7cf8217dd7ba3853176e051a26503a
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2024-05-31 15:07:31 -0400

    second commit

--- src/main/java/com/example/RevisionDemo/controller/StudentController.java
+++ src/main/java/com/example/RevisionDemo/controller/StudentController.java
@@ -1,6 +1,5 @@
 package com.example.RevisionDemo.controller;
 
-
 import com.example.RevisionDemo.entity.Student;
 import com.example.RevisionDemo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Controller class
 @RestController
 @RequestMapping("/api/student")
 @CrossOrigin
@@ -17,32 +17,30 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping
-    public List<Student> fetchAllStudents(){
+    public List<Student> fetchAllStudents() {
         return studentService.getAllStudents();
     }
 
     @GetMapping("/{id}")
-    public Student getSingleStudent(@PathVariable("id") long id){
+    public Student getSingleStudent(@PathVariable("id") long id) {
         return studentService.getSingleStudent(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable("id") long id){
+    public void deleteStudent(@PathVariable("id") long id) {
         studentService.deleteStudent(id);
     }
 
     @PostMapping
-    public void createStudent(@RequestBody Student student){
+    public void createStudent(@RequestBody Student student) {
 
         studentService.createStudent(student);
 
     }
 
     @PutMapping("/{id}")
-    public void updateStudent(@RequestBody Student student, @PathVariable("id") long id){
+    public void updateStudent(@RequestBody Student student, @PathVariable("id") long id) {
         studentService.updateStudent(id, student);
     }
-
-
 
 }
